4d1fc5c94873db78b8ea14db5faaea300c15900a
Bruno R.Novais <novais.rezende@gmail.com>
2021-02-26 15:16:12 -0300

    :bug: Deploy
    Rendered deploy bugs

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,20 +1,5 @@
-import { GetServerSideProps } from 'next';
-
-const PokemonPage: React.FC = (props) => {
+const PokemonPage: React.FC = () => {
   return <h1>Hello World</h1>;
 };
 
-type Data = {};
-
 export default PokemonPage;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const res = await fetch('https://.../data');
-  const data: Data = await res.json();
-
-  return {
-    props: {
-      data,
-    },
-  };
-};
